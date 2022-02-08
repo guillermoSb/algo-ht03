7e540165d5b3c81fd174a92705c696ca08e27bec
Arobelcam <aaronbeltrancampos@gmail.com>
2022-02-07 21:29:06 -0600

    Arreglos clase sorting
    Se ha cambiado el tipo de valor que retornara cada metodo de la clase

--- src/com/ht03/Main.java
+++ src/com/ht03/Main.java
@@ -2,6 +2,17 @@ package com.ht03;
 
 import java.util.ArrayList;
 
+/**
+ * Algoritmos y Estructuras de Datos
+ * Universidad del Valle de Guatemala
+ * Hoja de Trabajo No.3
+ * @file Main.java
+ * @author Aaron Beltran  21092
+ * @author Mariel Guamuche 21150
+ * @author Sebastian Reyes 21139
+ * @author Guillermo Santos 191517
+ */
+
 public class Main {
 
     public static void main(String[] args) {
--- src/com/ht03/Sorting.java
+++ src/com/ht03/Sorting.java
@@ -1,13 +1,41 @@
 package com.ht03;
 
+import java.util.ArrayList;
+
+/**
+ * Clase que contiene la implementacion de los metodos
+ * @since 07/febrero/2022
+ * @author Aaron Beltran  21092
+ * @author Mariel Guamuche 21150
+ * @author Sebastian Reyes 21139
+ * @author Guillermo Santos 191517
+ */
+
 public class Sorting {
 
+    /**
+     * Metodo realizar el ordenamiento tipo gnome sort
+     */
+    public ArrayList<Integer> Gnomesort(){
 
-    public void Gnomesort(){}
-
-    public void Mergesort(){}
-
-    public void Quicksort(){}
-
-    public void Radixsort(){}
+        return null;
+    }
+    /**
+     * Metodo para realizar el ordenamiento tipo merge sort
+     */
+    public ArrayList<Integer> Mergesort(){
+        return null;
+    }
+    /**
+     * Metodo  para realizar el ordenamiento tipo quick sort
+     */
+    public ArrayList<Integer> Quicksort(){
+        return null;
+    }
+    /**
+     * Metodo para realizar el ordenamiento tipo raidix sort
+     */
+    public ArrayList<Integer> Radixsort(){
+        return null;
+    }
 }
