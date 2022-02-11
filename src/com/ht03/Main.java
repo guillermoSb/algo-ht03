cb4c7c26781f3badecbb64fe8d2860c2e1c662f4
Guillermo <guillermo@inlandlogistics.co>
2022-02-10 23:12:57 -0600

    Removed unused variable

--- src/com/ht03/Main.java
+++ src/com/ht03/Main.java
@@ -20,7 +20,6 @@ public class Main {
     public static void main(String[] args) {
         FileManager manager = new FileManager();
         Sorting sorting = new Sorting();
-        int limite = 100;
         try {
             // Merge Sort
             ArrayList<Integer> sortedM =  sorting.mergeSort(manager.readFile("numbers.txt"));
